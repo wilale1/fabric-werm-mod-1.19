2bbf546dd32190e619a7a9b78190640c11e3a96c
Wilale <wilale1@hotmail.com>
2022-07-19 13:34:22 +1000

    Initial commit

---
+++ src/main/java/net/wilale1/wermmod/WermMod.java
@@ -0,0 +1,14 @@
+package net.wilale1.wermmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WermMod implements ModInitializer {
+	public static final String MOD_ID = "wermmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/wilale1/wermmod/WermModClient.java
@@ -0,0 +1,10 @@
+package net.wilale1.wermmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WermModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
